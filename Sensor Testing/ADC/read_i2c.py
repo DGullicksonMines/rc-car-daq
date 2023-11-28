ecabcffb81af788d4fbd75ef0844bce7080ccab5
Dawson Gullickson <dgullickson@mines.edu>
2023-11-28 12:44:28 -0700

    Made ADC testing loop

--- Sensor Testing/ADC/read_i2c.py
+++ Sensor Testing/ADC/read_i2c.py
@@ -8,7 +8,20 @@ import mcp3424
 # Initialize I2C bus at the standard mode rate
 bus = busio.I2C(board.SCL, board.SDA, frequency=100000)
 
-#TODO
+# ADC initial configuration
+ADDRESS = 0
+adc = mcp3424.ADC(bus, ADDRESS)
+adc.configure()
 
-# Close I2C bus
-bus.deinit()
+try:
+    # Allow user to probe channel voltages
+    while True:
+        channel = int(input("Channel"))
+        adc.update_and_configure(channel=channel)
+        voltage = adc.read()
+        print("Read {} V".format(voltage))
+        print()
+except KeyboardInterrupt: pass
+finally:
+    # Close I2C bus
+    bus.deinit()
