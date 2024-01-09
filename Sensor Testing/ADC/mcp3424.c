a6b96e4c464f3deadd403fde311f06a1de08e3e0
Dawson Gullickson <dgullickson@mines.edu>
2024-01-09 14:35:18 -0700

    Created basic C library for using ADC.

---
+++ Sensor Testing/ADC/mcp3424.c
@@ -0,0 +1,80 @@
+#include "mcp3424.h"
+
+#include <fcntl.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+
+uint8_t construct_config_byte(Config config) {
+    uint8_t byte = 0;
+    byte |= ((config.ready) ? 1:0) << 7;
+    byte |= config.channel << 5;
+    byte |= ((config.continuous_mode) ? 1:0) << 4;
+    byte |= config.sample_rate << 2;
+    byte |= config.gain;
+    return byte;
+}
+
+Config parse_config_byte(uint8_t byte) {
+    Config config;
+    config.ready = (byte >> 7) == 1;
+    config.channel = ((byte >> 5) % 4);
+    config.continuous_mode = (byte >> 4) % 2 == 1;
+    config.sample_rate = (byte >> 2) % 4;
+    config.gain = byte % 4;
+    return config;
+}
+
+
+uint8_t construct_address_byte(uint8_t address) {
+    uint8_t byte = 0b1101 << 3; // Set by manufacturer
+    byte |= address;
+    return byte;
+}
+
+int _ADC_set_slave_addr(ADC adc) {
+    uint8_t addr = construct_address_byte(adc.address);
+    return ioctl(adc.bus, I2C_SLAVE, addr);
+}
+
+int ADC_init(ADC *adc, const char *i2c_bus, uint8_t address) {
+    // Open bus
+    int bus = open(i2c_bus, O_RDWR);
+    if (bus < 0) return bus; // Error
+    
+    // Initialize
+    adc->bus = bus;
+    adc->address = address;
+
+    return 0;
+}
+
+int ADC_configure(ADC adc) {
+    uint8_t config = construct_config_byte(adc.config);
+    uint8_t buffer[1] = { config };
+    if (_ADC_set_slave_addr(adc) < 0) return -1;
+    if (write(adc.bus, config, 1) != 1) return -2;
+    return 0;
+}
+
+int ADC_read(ADC adc, double *value) {
+    // Read 3 bytes from ADC
+    uint8_t buffer[3];
+    if (_ADC_set_slave_addr(adc) < 0) return -1;
+    if (read(adc.bus, buffer, 3) != 3) return -2;
+
+    // Combine bytes
+    uint32_t data = buffer[2];
+    data |= ((uint32_t)buffer[1]) << 8;
+    data |= ((uint32_t)buffer[0]) << 16;
+
+    // Convert to voltage
+    uint8_t bits = bits_per_rate(adc.config.sample_rate);
+    double converted = (
+        ((double)data / (1 << bits))
+        * (FULL_SCALE_RANGE / adc.config.gain)
+    );
+
+    return converted;
+}
---
+++ Sensor Testing/ADC/mcp3424.h
@@ -0,0 +1,61 @@
+#ifndef MCP3424_H
+#define MCP3424_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+const double FULL_SCALE_RANGE = 2.048;
+
+typedef enum {
+    HZ240 = 0,
+    HZ60 = 1,
+    HZ15 = 2,
+    HZ3_75 = 3,
+} SampleRate;
+
+uint8_t bits_per_rate(SampleRate sample_rate) {
+    switch (sample_rate) {
+    case HZ240: return 12;
+    case HZ60: return 14;
+    case HZ15: return 16;
+    case HZ3_75: return 18;
+    }
+}
+
+typedef enum {
+    CH1 = 0,
+    CH2 = 1,
+    CH3 = 2,
+    CH4 = 3,
+} Channel;
+
+typedef struct {
+    Channel channel; // 2 bits
+    bool ready;
+    bool continuous_mode;
+    SampleRate sample_rate; // 2 bits
+    uint8_t gain; // 2 bits
+} Config;
+
+uint8_t construct_config_byte(Config config);
+
+Config parse_config_byte(uint8_t byte);
+
+/// @brief Constructs an address byte for the MCP3424.
+/// @param address 3-bit address of MCP3424
+/// @return An address byte.
+uint8_t construct_address_byte(uint8_t address);
+
+typedef struct {
+    int bus;
+    uint8_t address;
+    Config config;
+} ADC;
+
+int ADC_init(ADC *adc, const char *i2c_bus, uint8_t address);
+
+int ADC_configure(ADC adc);
+
+int ADC_read(ADC adc, double *value);
+
+#endif
