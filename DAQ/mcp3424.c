29f987705a16f362b898bbf71aea00698af90754
Dawson Gullickson <dgullickson@mines.edu>
2024-03-05 10:14:26 -0700

    MCP library for main DAQ

---
+++ DAQ/mcp3424.c
@@ -0,0 +1,113 @@
+// Copyright 2024 by Dawson J. Gullickson
+
+#include "mcp3424.h"
+
+#include <fcntl.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+
+const double FULL_SCALE_RANGE = 2.048;
+
+uint8_t construct_config_byte(Config config) {
+    uint8_t byte = 0;
+    byte |= ((config.ready) ? 0:1) << 7;
+    byte |= config.channel << 5;
+    byte |= ((config.continuous_mode) ? 1:0) << 4;
+    byte |= config.sample_rate << 2;
+    byte |= config.gain;
+    return byte;
+}
+
+Config parse_config_byte(uint8_t byte) {
+    Config config;
+    config.ready = (byte >> 7) == 0;
+    config.channel = ((byte >> 5) % 4);
+    config.continuous_mode = (byte >> 4) % 2 == 1;
+    config.sample_rate = (byte >> 2) % 4;
+    config.gain = byte % 4;
+    return config;
+}
+
+
+uint8_t construct_address_byte(uint8_t address) {
+    uint8_t byte = 0b1101 << 3; // Set by manufacturer
+    byte |= address;
+    return byte;
+}
+
+int _ADC_set_slave_addr(ADC adc) {
+    uint8_t addr = construct_address_byte(adc.address);
+    return ioctl(adc.bus, I2C_SLAVE, addr);
+}
+
+int ADC_init(ADC *adc, const char *i2c_bus, uint8_t address) {
+    // Open bus
+    int bus = open(i2c_bus, O_RDWR);
+    if (bus < 0) return bus; // Error
+    
+    // Initialize
+    adc->bus = bus;
+    adc->address = address;
+
+    return 0;
+}
+
+void ADC_deinit(ADC adc) {
+    close(adc.bus);
+}
+
+BitMode ADC_bit_mode(ADC adc) {
+    if (!adc.config.continuous_mode) return Bits18;
+    switch (adc.config.sample_rate) {
+    case HZ240: return Bits12;
+    case HZ60: return Bits14;
+    case HZ15: return Bits16;
+    case HZ3_75: return Bits18;
+    }
+}
+
+int ADC_configure(ADC adc) {
+    uint8_t config = construct_config_byte(adc.config);
+    uint8_t buffer[1] = { config };
+    if (_ADC_set_slave_addr(adc) < 0) return -1;
+    if (write(adc.bus, buffer, 1) != 1) return -2;
+    return 0;
+}
+
+int ADC_read(ADC adc, double *value) {
+    // Get bit mode
+    BitMode bit_mode = ADC_bit_mode(adc);
+
+    // Read data bytes and config byte from ADC
+    // Data bytes: 3 in 18-bit mode, 2 otherwise
+    if (_ADC_set_slave_addr(adc) < 0) return -2;
+    uint8_t buffer[4];
+    ssize_t read_result;
+    if (bit_mode == Bits18) {
+        if (read(adc.bus, buffer, 4) != 4) return -1;
+    } else {
+        buffer[0] = 0;
+        if (read(adc.bus, &buffer[1], 3) != 3) return -1;
+    }
+    
+    // Combine bytes
+    uint32_t data = buffer[2];
+    data |= ((uint32_t)buffer[1]) << 8;
+    data |= ((uint32_t)buffer[0]) << 16;
+    data >>= 2; //XXX Unsure why this is necessary
+    data &= ((uint32_t)1 << bit_mode) - 1;
+
+    // Convert to voltage
+    int32_t data_signed = data & (((uint32_t)1 << bit_mode) - 1);
+    data_signed -= (data_signed & ((uint32_t)1 << (bit_mode - 1))) << 1;
+    double LSB = FULL_SCALE_RANGE / ((uint32_t)1 << (bit_mode - 1));
+    uint8_t gain = (uint32_t)1 << adc.config.gain;
+    *value = data_signed * (LSB / gain);
+
+    // Check if sample was up to date
+    Config config = parse_config_byte(buffer[3]);
+    if (config.ready) return 1;
+    return 0;
+}
---
+++ DAQ/mcp3424.h
@@ -0,0 +1,72 @@
+// Copyright 2024 by Dawson J. Gullickson
+
+#ifndef MCP3424_H
+#define MCP3424_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+extern const double FULL_SCALE_RANGE;
+
+typedef enum {
+    HZ240 = 0,
+    HZ60 = 1,
+    HZ15 = 2,
+    HZ3_75 = 3,
+} SampleRate;
+
+typedef enum {
+    Bits12 = 12,
+    Bits14 = 14,
+    Bits16 = 16,
+    Bits18 = 18,
+} BitMode;
+
+typedef enum {
+    CH1 = 0,
+    CH2 = 1,
+    CH3 = 2,
+    CH4 = 3,
+} Channel;
+
+typedef struct {
+    Channel channel; // 2 bits
+    bool ready; // 1 bit
+    bool continuous_mode; // 1 bit
+    SampleRate sample_rate; // 2 bits
+    uint8_t gain; // 2 bits, logarithmic
+} Config;
+
+uint8_t construct_config_byte(Config config);
+
+Config parse_config_byte(uint8_t byte);
+
+/// @brief Constructs an address byte for the MCP3424.
+/// @param address 3-bit address of MCP3424
+/// @return An address byte.
+uint8_t construct_address_byte(uint8_t address);
+
+typedef struct {
+    int bus;
+    uint8_t address;
+    Config config;
+} ADC;
+
+int ADC_init(ADC *adc, const char *i2c_bus, uint8_t address);
+
+void ADC_deinit(ADC adc);
+
+BitMode ADC_bit_mode(ADC adc);
+
+int ADC_configure(ADC adc);
+
+/// @brief Reads a voltage from an ADC into `value`
+/// @param adc An ADC struct
+/// @param value Pointer voltage is read into
+/// @return
+/// -2: Failed to set ADC as I2C slave
+/// -1: Failed to read from the ADC
+///  0: 
+int ADC_read(ADC adc, double *value);
+
+#endif
