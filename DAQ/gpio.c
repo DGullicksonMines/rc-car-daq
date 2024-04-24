2b4fddce3c4107a309cb427fd8941de78f76964e
Dawson Gullickson <dgullickson@mines.edu>
2024-04-24 17:06:11 -0600

    Still not working

--- DAQ/gpio.c
+++ DAQ/gpio.c
@@ -57,7 +57,9 @@ void *_polling(void *args) {
 		printf("reading value \n");
 		struct gpio_v2_line_values values;
 		values.mask = 1 << event.offset;
-		if (ioctl(poll_fd.fd, GPIO_V2_LINE_GET_VALUES_IOCTL, &values) < 0)
+		int res = ioctl(poll_fd.fd, GPIO_V2_LINE_GET_VALUES_IOCTL, &values)
+		printf("res: %d \n", res);
+		if (res < 0)
 			return (void *)-4;
 		bool active = (values.bits & (1 << event.offset)) > 0;
 		// Call interrupt
