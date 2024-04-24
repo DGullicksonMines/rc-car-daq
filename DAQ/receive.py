cc1da30f4bd84b949b0c21ab6c77ffee3d14b34b
trdolan <trdolan@mines.edu>
2024-04-24 10:29:20 -0600

    Fixed spelling error in receive.py

--- DAQ/receive.py
+++ DAQ/receive.py
@@ -37,7 +37,7 @@ class Entry:
 	def write_header(writer: t.TextIO):
 		writer.write("time,cell0,cell1,cell2,cell3,cell4,cell5,")
 		writer.write("acc_x,acc_y,acc_z,roll,pitch,yaw,")
-		writer.write("latitutde,longitude,satellites,speed,")
+		writer.write("latitude,longitude,satellites,speed,")
 		writer.write("rpm_bl,rpm_br,rpm_fl,rpm_fr,") #TODO order
 		writer.write("steering \n")
 
